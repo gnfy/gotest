9901233689d48e4df692f2c30d496249c2ec1c1d
gnfy <gnfy@163.com>
2022-09-13 23:36:17 +0800

    新增单词出现次数:lips:

---
+++ test_find_word.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// 查询单词出现次数
+	var str = "how do you do"
+	strSlice := strings.Split(str, " ")
+	var strMap = make(map[string]int)
+	for _, v := range strSlice {
+		strMap[v]++
+	}
+	fmt.Println("strMap: ", strMap)
+}
