4f6189d768e1cb919c3a4ded1ab80d606d689c6f
gnfy <gnfy@163.com>
2022-09-17 01:35:35 +0800

    时间格式化显示:eyes:

---
+++ test_time.go
@@ -0,0 +1,15 @@
+/* package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	uTime := time.Now()
+	timeObj := time.Unix(uTime.Unix(), 0)
+	// 格式化时间显示 2006 1 2 3 4 5，其中3表示12小时制，15表示24小时制
+	str := timeObj.Format("2006-01-02 15:04:05")
+	fmt.Println("str: ", str)
+}
+*/
